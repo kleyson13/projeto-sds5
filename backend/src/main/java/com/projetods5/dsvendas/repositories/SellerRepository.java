7551d0ce63efd34a72593aef6dc9627e09fae7b7
Kleyson Mariano <kleysons913@gmail.com>
2021-11-06 16:32:56 -0300

    Layers

---
+++ backend/src/main/java/com/projetods5/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.projetods5.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetods5.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
